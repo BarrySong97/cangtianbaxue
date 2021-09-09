27396410dfd7a4444923fb4b8796843d3ea878f4
barry song <524000659@qq.com>
2021-09-09 22:12:36 +0800

    feature:设计小说目录和章节内容的布局

--- src/pages/fiction/index.tsx
+++ src/pages/fiction/index.tsx
@@ -4,10 +4,10 @@ import {
   getFictionContentByUrl,
 } from '@/service';
 import { useBoolean, useRequest } from 'ahooks';
-import { Button } from 'antd';
+import { Button, List } from 'antd';
 import React, { FC, useState } from 'react';
-import { useParams, useLocation } from 'react-router';
-import ContentModal from './components/content-modal';
+import parse from 'html-react-parser';
+import { useLocation } from 'react-router';
 export interface FictionProps {}
 const Fiction: FC<FictionProps> = () => {
   const location = useLocation();
@@ -16,7 +16,7 @@ const Fiction: FC<FictionProps> = () => {
 
   const [fictionCatalog, setFictionCatalog] = useState<FictionCatalog[]>([]);
   const [content, setContent] = useState<string>('');
-  useRequest(() => getFictionCatalogByUrl(query.url), {
+  const { loading } = useRequest(() => getFictionCatalogByUrl(query.url), {
     onSuccess: (data) => {
       setFictionCatalog(data);
     },
@@ -29,25 +29,34 @@ const Fiction: FC<FictionProps> = () => {
     },
   });
   return (
-    <div>
-      {fictionCatalog.map((v) => (
-        <Button
-          type="link"
-          key={v.name}
-          onClick={() => {
-            getContentRequest(v.link);
+    <div className="flex">
+      <div className="h-screen overflow-scroll">
+        <List
+          size="small"
+          pagination={{
+            pageSize: Math.floor(window.innerHeight / 49 - 1),
           }}
-        >
-          {v.name}
-        </Button>
-      ))}
+          loading={loading}
+          dataSource={fictionCatalog}
+          renderItem={(item) => (
+            <List.Item>
+              <Button
+                type="link"
+                key={item.name}
+                onClick={() => {
+                  getContentRequest(item.link);
+                }}
+              >
+                {item.name}
+              </Button>
+            </List.Item>
+          )}
+        />
+      </div>
 
-      <ContentModal
-        visible={state}
-        content={content}
-        onCancel={setFalse}
-        onOk={setTrue}
-      />
+      <div className="h-screen flex-1 overflow-scroll p-4">
+        {parse(content)}
+      </div>
     </div>
   );
 };
